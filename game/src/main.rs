49b43a3d7936d7d6a7fc8f78f347a0140e089aff
Zstorm999 <46458357+Zstorm999@users.noreply.github.com>
2022-10-29 17:11:28 +0200

    Draw edges of polygons

--- game/src/main.rs
+++ game/src/main.rs
@@ -1,7 +1,8 @@
+mod polygon;
+
 use bevy::prelude::*;
-use bevy::render::mesh::Indices;
-use bevy::render::render_resource::PrimitiveTopology;
-use bevy::sprite::MaterialMesh2dBundle;
+
+use polygon::Polygon;
 
 fn main() {
     App::new()
@@ -17,121 +18,14 @@ fn setup(
 ) {
     commands.spawn_bundle(Camera2dBundle::default());
 
-    let mesh = mesh_from_polygon(&vec![
+    let points: Vec<_> = vec![
         [0.5, 0.0],
         [0.5, 0.1],
         [0.42, 0.71],
         [0.0, 1.0],
         [-0.5, -0.5],
-    ]);
+    ];
+    let mesh: Polygon = (&points[..]).into();
 
-    commands.spawn_bundle(MaterialMesh2dBundle {
-        mesh: meshes.add(mesh).into(),
-        transform: Transform::default().with_scale(Vec3::splat(128.)),
-        material: materials.add(ColorMaterial::from(Color::PURPLE)),
-        ..default()
-    });
-}
-
-/**
-   Creates a mesh from the vertices of a polygon.
-
-   This function expects the polygon to be convex, centred around (0, 0) and the points to be presented in trigonometric order.
-   The position of the first point is however irrelevant (it doesn’t have to lie on any of the two axes).
-
-   # Panics
-
-   Panics if less than 3 points are provided.
-*/
-fn mesh_from_polygon(points: &[[f32; 2]]) -> Mesh {
-    assert!(points.len() > 2);
-
-    let mut points_3d: Vec<_> = Vec::with_capacity(points.len());
-    let mut normals: Vec<_> = Vec::with_capacity(points.len());
-    let mut uvs: Vec<_> = Vec::with_capacity(points.len());
-
-    for p in points {
-        points_3d.push([p[0], p[1], 0.0]);
-        normals.push([0.0, 0.0, 1.0]);
-        uvs.push([0.0, 0.0]);
-    }
-
-    let nb_triangles = points.len() - 2;
-
-    let indices: Vec<_> = (1..)
-        .map(|i| [0, i, i + 1])
-        .take(nb_triangles)
-        .flatten()
-        .collect();
-
-    println!("{:?}", indices);
-
-    let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
-    mesh.set_indices(Some(Indices::U32(indices)));
-    mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, points_3d);
-    mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, normals);
-    mesh.insert_attribute(Mesh::ATTRIBUTE_UV_0, uvs);
-    mesh
-}
-
-#[cfg(test)]
-mod tests {
-
-    use super::*;
-
-    #[test]
-    #[should_panic]
-    fn panics_if_less_than_three_points() {
-        let _ = mesh_from_polygon(&[]);
-    }
-
-    fn test_shape(points: &[[f32; 2]]) {
-        let shape = mesh_from_polygon(&points);
-
-        shape
-            .attribute(Mesh::ATTRIBUTE_POSITION)
-            .unwrap()
-            .as_float3()
-            .unwrap()
-            .iter()
-            .zip(points.iter())
-            .for_each(|(new, original)| {
-                assert_eq!(&new[0..2], original);
-                assert_eq!(new[2], 0.0);
-            });
-
-        shape
-            .attribute(Mesh::ATTRIBUTE_NORMAL)
-            .unwrap()
-            .as_float3()
-            .unwrap()
-            .iter()
-            .for_each(|n| assert_eq!(n, &[0.0, 0.0, 1.0]));
-
-        shape
-            .indices()
-            .unwrap()
-            .iter()
-            .zip(
-                (1..)
-                    .map(|i| [0, i, i + 1])
-                    .take(points.len() - 2)
-                    .flatten(),
-            )
-            .for_each(|(new, original)| assert_eq!(new, original))
-
-        // don’t test uvs since we’re only using single colours for now
-    }
-
-    #[test]
-    fn correctly_builds_triangle() {
-        let points = [[0.0, 1.0], [-1.0, 0.0], [1.0, 0.0]];
-        test_shape(&points);
-    }
-
-    #[test]
-    fn correctly_builds_square() {
-        let points = [[-0.5, -0.5], [0.5, -0.5], [0.5, 0.5], [-0.5, 0.5]];
-        test_shape(&points);
-    }
+    mesh.draw(&mut commands, &mut meshes, &mut materials);
 }
---
+++ game/src/polygon.rs
@@ -0,0 +1,153 @@
+use bevy::prelude::*;
+
+use bevy::render::mesh::Indices;
+use bevy::render::render_resource::PrimitiveTopology;
+use bevy::sprite::MaterialMesh2dBundle;
+
+/**
+    Convex polygon
+
+   The polygon is expected to be centred around (0,0) and convex.
+   No verification is performed on the convexity, this is left up to the user
+*/
+pub struct Polygon {
+    polygon: Mesh,
+    contours: Mesh,
+}
+
+impl Polygon {
+    pub fn draw(
+        &self,
+        commands: &mut Commands,
+        meshes: &mut ResMut<Assets<Mesh>>,
+        materials: &mut ResMut<Assets<ColorMaterial>>,
+    ) {
+        commands.spawn_bundle(MaterialMesh2dBundle {
+            mesh: meshes.add(self.polygon.clone()).into(),
+            transform: Transform::default().with_scale(Vec3::splat(128.0)),
+            material: materials.add(ColorMaterial::from(Color::PURPLE)),
+            ..default()
+        });
+
+        commands.spawn_bundle(MaterialMesh2dBundle {
+            mesh: meshes.add(self.contours.clone()).into(),
+            transform: Transform::default().with_scale(Vec3::splat(128.0)),
+            material: materials.add(ColorMaterial::from(Color::BLACK)),
+            ..default()
+        });
+    }
+}
+
+impl From<&[[f32; 2]]> for Polygon {
+    fn from(points: &[[f32; 2]]) -> Self {
+        assert!(points.len() > 2);
+
+        let mut points_3d: Vec<_> = Vec::with_capacity(points.len());
+        let mut normals: Vec<_> = Vec::with_capacity(points.len());
+        let mut uvs: Vec<_> = Vec::with_capacity(points.len());
+
+        for p in points {
+            points_3d.push([p[0], p[1], 0.0]);
+            normals.push([0.0, 0.0, 1.0]);
+            uvs.push([0.0, 0.0]);
+        }
+
+        let nb_triangles = points.len() - 2;
+
+        let indices: Vec<_> = (1..)
+            .map(|i| [0, i, i + 1])
+            .take(nb_triangles)
+            .flatten()
+            .collect();
+
+        println!("{:?}", indices);
+
+        let mut polygon = Mesh::new(PrimitiveTopology::TriangleList);
+        polygon.set_indices(Some(Indices::U32(indices)));
+        polygon.insert_attribute(Mesh::ATTRIBUTE_POSITION, points_3d.clone());
+        polygon.insert_attribute(Mesh::ATTRIBUTE_NORMAL, normals.clone());
+        polygon.insert_attribute(Mesh::ATTRIBUTE_UV_0, uvs.clone());
+
+        points_3d.push(points_3d[0]);
+        normals.push(normals[0]);
+        uvs.push(uvs[0]);
+
+        println!("{:?}", points_3d);
+        println!("{:?}", normals);
+        println!("{:?}", uvs);
+
+        let mut contours = Mesh::new(PrimitiveTopology::LineStrip);
+        contours.insert_attribute(Mesh::ATTRIBUTE_POSITION, points_3d);
+        contours.insert_attribute(Mesh::ATTRIBUTE_NORMAL, normals);
+        contours.insert_attribute(Mesh::ATTRIBUTE_UV_0, uvs);
+
+        Polygon { polygon, contours }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    #[should_panic]
+    fn panics_if_less_than_three_points() {
+        let points = vec![];
+
+        let _: Polygon = (&points[..]).into();
+    }
+
+    fn test_shape(points: &[[f32; 2]]) {
+        let shape: Polygon = points.into();
+
+        shape
+            .polygon
+            .attribute(Mesh::ATTRIBUTE_POSITION)
+            .unwrap()
+            .as_float3()
+            .unwrap()
+            .iter()
+            .zip(points.iter())
+            .for_each(|(new, original)| {
+                assert_eq!(&new[0..2], original);
+                assert_eq!(new[2], 0.0);
+            });
+
+        shape
+            .polygon
+            .attribute(Mesh::ATTRIBUTE_NORMAL)
+            .unwrap()
+            .as_float3()
+            .unwrap()
+            .iter()
+            .for_each(|n| assert_eq!(n, &[0.0, 0.0, 1.0]));
+
+        shape
+            .polygon
+            .indices()
+            .unwrap()
+            .iter()
+            .zip(
+                (1..)
+                    .map(|i| [0, i, i + 1])
+                    .take(points.len() - 2)
+                    .flatten(),
+            )
+            .for_each(|(new, original)| assert_eq!(new, original))
+
+        // don’t test uvs since we’re only using single colours for now
+    }
+
+    #[test]
+    fn correctly_builds_triangle() {
+        let points = [[0.0, 1.0], [-1.0, 0.0], [1.0, 0.0]];
+        test_shape(&points);
+    }
+
+    #[test]
+    fn correctly_builds_square() {
+        let points = [[-0.5, -0.5], [0.5, -0.5], [0.5, 0.5], [-0.5, 0.5]];
+        test_shape(&points);
+    }
+}
